8d44c06d6683866530c2ec4a6fda25a074a4d91b
YasirHussain <yasirhussaints@outlook.com>
2024-11-28 21:02:47 +0530

    PubLib App Backend Completed

--- backend/publibapp/src/main/java/com/publib/app/controller/BookController.java
+++ backend/publibapp/src/main/java/com/publib/app/controller/BookController.java
@@ -5,9 +5,11 @@ import com.publib.app.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
